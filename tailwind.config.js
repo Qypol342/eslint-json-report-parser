bee20c457d51d6f8f523daa596ffa159a2f786c8
Qypol342 <Qypol342@gmail.com>
2023-10-12 22:55:23 +0200

    style(project): :lipstick: dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "purple-1000":"#23013d",
+      }
+    },
   },
   plugins: [],
 }
